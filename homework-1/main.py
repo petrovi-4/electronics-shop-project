f6af537c63ba3587a57bab2c09b5ca84858c5928
petrovi4 <40.10.444@gmail.com>
2024-01-24 12:40:41 +0300

    release item.py

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,5 +1,6 @@
 from src.item import Item
 
+
 if __name__ == '__main__':
     item1 = Item("Смартфон", 10000, 20)
     item2 = Item("Ноутбук", 20000, 5)
@@ -15,4 +16,5 @@ if __name__ == '__main__':
     print(item1.price)  # 8000.0
     print(item2.price)  # 20000
 
-    print(Item.all)  # [<__main__.Item object at 0x000001EC6250C690>, <__main__.Item object at 0x000001EC6250C6D0>]
+    print(Item.all)  # [<__main__.Item object at 0x000001EC6250C690>,
+    # <__main__.Item object at 0x000001EC6250C6D0>]
